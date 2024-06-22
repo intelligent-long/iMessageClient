e85b55ee63430dcd5916bf025acc9fb86cac08da
long <tx.long@outlook.com>
2024-06-23 03:25:25 +0800

    commit

--- app/src/main/java/com/longx/intelligent/android/ichat2/activity/PreviewToSendVideoActivity.java
+++ app/src/main/java/com/longx/intelligent/android/ichat2/activity/PreviewToSendVideoActivity.java
@@ -3,18 +3,23 @@ package com.longx.intelligent.android.ichat2.activity;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.SeekBar;
 
 import com.longx.intelligent.android.ichat2.R;
 import com.longx.intelligent.android.ichat2.activity.helper.BaseActivity;
 import com.longx.intelligent.android.ichat2.databinding.ActivityPreviewToSendVideoBinding;
 import com.longx.intelligent.android.ichat2.media.helper.MediaStoreHelper;
 import com.longx.intelligent.android.ichat2.util.ColorUtil;
+import com.longx.intelligent.android.ichat2.util.ErrorLogger;
 import com.longx.intelligent.android.ichat2.util.WindowAndSystemUiUtil;
 
 import org.videolan.libvlc.LibVLC;
 import org.videolan.libvlc.Media;
 import org.videolan.libvlc.MediaPlayer;
 import org.videolan.libvlc.interfaces.IMedia;
+
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class PreviewToSendVideoActivity extends BaseActivity {
     private ActivityPreviewToSendVideoBinding binding;
@@ -24,6 +29,8 @@ public class PreviewToSendVideoActivity extends BaseActivity {
     private long savedPosition = -1;
     private boolean isMediaPrepared;
     private boolean pureVideo;
+    private Timer timer;
+    private static final int SEEKBAR_MAX = 100;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -61,11 +68,13 @@ public class PreviewToSendVideoActivity extends BaseActivity {
                 case MediaPlayer.Event.Paused:{
                     binding.pauseButton.setVisibility(View.GONE);
                     binding.playButton.setVisibility(View.VISIBLE);
+                    stopProgressUpdate();
                     break;
                 }
                 case MediaPlayer.Event.Playing:{
                     binding.pauseButton.setVisibility(View.VISIBLE);
                     binding.playButton.setVisibility(View.GONE);
+                    startProgressUpdate();
                     break;
                 }
                 case MediaPlayer.Event.EncounteredError:
@@ -78,6 +87,8 @@ public class PreviewToSendVideoActivity extends BaseActivity {
                     isMediaPrepared = false;
                     binding.pauseButton.setVisibility(View.GONE);
                     binding.playButton.setVisibility(View.VISIBLE);
+                    stopProgressUpdate();
+                    runOnUiThread(() -> {binding.seekbar.setProgress(binding.seekbar.getMax());});
                     break;
             }
         });
@@ -93,6 +104,66 @@ public class PreviewToSendVideoActivity extends BaseActivity {
             }
         });
         binding.pauseButton.setOnClickListener(v -> mediaPlayer.pause());
+        binding.seekbar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                if (fromUser) {
+                    if (!isMediaPrepared) {
+                        if (realPath != null) {
+                            Media media = new Media(libVLC, realPath);
+                            mediaPlayer.setMedia(media);
+                            mediaPlayer.play();
+                        }
+                    }
+                    mediaPlayer.setTime((long) ((progress / (double) SEEKBAR_MAX) * mediaPlayer.getLength()));
+                }
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+                stopProgressUpdate();
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+                startProgressUpdate();
+            }
+        });
+    }
+
+    private void startProgressUpdate() {
+        if (timer != null) {
+            timer.cancel();
+        }
+        timer = new Timer();
+        timer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                updateSeekbarPosition();
+            }
+        }, 0, 1000);
+    }
+
+    private void updateSeekbarPosition() {
+        runOnUiThread(() -> {
+            if (mediaPlayer != null) {
+                binding.seekbar.setMax(SEEKBAR_MAX);
+                long currentPosition = mediaPlayer.getTime();
+                long duration = mediaPlayer.getLength();
+                int position = (int) ((currentPosition / (double) duration) * SEEKBAR_MAX);
+                binding.seekbar.setProgress(position);
+            }
+        });
+    }
+
+    private void stopProgressUpdate() {
+        if (timer != null) {
+            timer.cancel();
+            timer = null;
+        }
+        try {
+            updateSeekbarPosition();
+        }catch (Exception ignore){}
     }
 
     @Override
@@ -103,6 +174,7 @@ public class PreviewToSendVideoActivity extends BaseActivity {
             savedPosition = (int) mediaPlayer.getTime();
             mediaPlayer.pause();
         }
+        stopProgressUpdate();
     }
 
     @Override
@@ -126,6 +198,7 @@ public class PreviewToSendVideoActivity extends BaseActivity {
         if (libVLC != null) {
             libVLC.release();
         }
+        stopProgressUpdate();
     }
 
     private void setupYiers() {
