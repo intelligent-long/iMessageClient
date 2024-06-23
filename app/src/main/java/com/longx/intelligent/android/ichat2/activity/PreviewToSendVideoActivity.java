03d8c5684b8b499ba9062bd64fa17b74f506ad5b
long <tx.long@outlook.com>
2024-06-23 19:48:34 +0800

    commit

--- app/src/main/java/com/longx/intelligent/android/ichat2/activity/PreviewToSendVideoActivity.java
+++ app/src/main/java/com/longx/intelligent/android/ichat2/activity/PreviewToSendVideoActivity.java
@@ -2,35 +2,31 @@ package com.longx.intelligent.android.ichat2.activity;
 
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
 import android.widget.SeekBar;
 
+import androidx.annotation.NonNull;
+
+import com.google.android.exoplayer2.ExoPlayer;
+import com.google.android.exoplayer2.MediaItem;
+import com.google.android.exoplayer2.Player;
+import com.google.android.exoplayer2.Tracks;
 import com.longx.intelligent.android.ichat2.R;
 import com.longx.intelligent.android.ichat2.activity.helper.BaseActivity;
 import com.longx.intelligent.android.ichat2.databinding.ActivityPreviewToSendVideoBinding;
-import com.longx.intelligent.android.ichat2.media.helper.MediaStoreHelper;
 import com.longx.intelligent.android.ichat2.util.ColorUtil;
+import com.longx.intelligent.android.ichat2.util.ErrorLogger;
 import com.longx.intelligent.android.ichat2.util.TimeUtil;
 import com.longx.intelligent.android.ichat2.util.WindowAndSystemUiUtil;
-
-import org.videolan.libvlc.LibVLC;
-import org.videolan.libvlc.Media;
-import org.videolan.libvlc.MediaPlayer;
-import org.videolan.libvlc.interfaces.IMedia;
-
-import java.util.Timer;
-import java.util.TimerTask;
 
 public class PreviewToSendVideoActivity extends BaseActivity {
     private ActivityPreviewToSendVideoBinding binding;
     private Uri uri;
-    private LibVLC libVLC;
-    private MediaPlayer mediaPlayer;
-    private long savedPosition = -1;
-    private boolean isMediaPrepared;
-    private boolean pureVideo;
-    private Timer timer;
-    private static final int SEEKBAR_MAX = 1000;
+    private ExoPlayer player;
+    private Handler handler;
+    private Runnable updateProgressAction;
+    private static final int SEEKBAR_MAX = 10000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,191 +38,104 @@ public class PreviewToSendVideoActivity extends BaseActivity {
         setupDefaultBackNavigation(binding.toolbar, getColor(R.color.white));
         uri = getIntent().getParcelableExtra(ExtraKeys.URI);
         binding.appBarLayout.bringToFront();
+
         initializePlayer();
-        setupYiers();
+        initializeSeekBar();
     }
 
     private void initializePlayer() {
-        libVLC = new LibVLC(this);
-        mediaPlayer = new MediaPlayer(libVLC);
-        String realPath = MediaStoreHelper.getRealPathFromURI(this, uri);
-        if (realPath != null) {
-            Media media = new Media(libVLC, realPath);
-            mediaPlayer.setMedia(media);
-        }
-        mediaPlayer.setEventListener(event -> {
-            switch (event.type){
-                case MediaPlayer.Event.Vout: {
-                    IMedia.VideoTrack videoTrack = mediaPlayer.getCurrentVideoTrack();
-                    if (videoTrack != null) {
-                        int videoWidth = videoTrack.width;
-                        int videoHeight = videoTrack.height;
-                        binding.videoLayout.setVideoSize(videoWidth, videoHeight);
-                    }
-                    break;
-                }
-                case MediaPlayer.Event.Paused:{
-                    binding.pauseButton.setVisibility(View.GONE);
-                    binding.playButton.setVisibility(View.VISIBLE);
-                    stopProgressUpdate();
-                    break;
-                }
-                case MediaPlayer.Event.Playing:{
+        player = new ExoPlayer.Builder(this).build();
+        binding.playerView.setPlayer(player);
+        binding.playerView.setVisibility(View.GONE);
+        MediaItem mediaItem = MediaItem.fromUri(uri);
+        player.setMediaItem(mediaItem);
+        player.prepare();
+        player.play();
+
+        player.addListener(new Player.Listener() {
+            @Override
+            public void onIsPlayingChanged(boolean isPlaying) {
+                if (isPlaying) {
+                    binding.playerView.setVisibility(View.VISIBLE);
+                    startProgressUpdates();
                     binding.pauseButton.setVisibility(View.VISIBLE);
                     binding.playButton.setVisibility(View.GONE);
-                    startProgressUpdate();
-                    break;
-                }
-                case MediaPlayer.Event.EncounteredError:
-                    isMediaPrepared = false;
-                    break;
-                case MediaPlayer.Event.Opening:
-                    isMediaPrepared = true;
-                    break;
-                case MediaPlayer.Event.EndReached:
-                    isMediaPrepared = false;
+                } else {
+                    stopProgressUpdates();
                     binding.pauseButton.setVisibility(View.GONE);
                     binding.playButton.setVisibility(View.VISIBLE);
-                    stopProgressUpdate();
-                    updateSeekbarPositionAndTimePlayToEnd();
-                    break;
-            }
-        });
-        binding.playButton.setOnClickListener(v -> {
-            if (!isMediaPrepared) {
-                if (realPath != null) {
-                    Media media = new Media(libVLC, realPath);
-                    mediaPlayer.setMedia(media);
-                    mediaPlayer.play();
                 }
-            } else {
-                mediaPlayer.play();
+            }
+
+            @Override
+            public void onPlaybackStateChanged(int playbackState) {
+                if (playbackState == Player.STATE_ENDED) {
+                    stopProgressUpdates();
+                }
             }
         });
-        binding.pauseButton.setOnClickListener(v -> mediaPlayer.pause());
+
+        binding.playButton.setOnClickListener(v -> {
+            if (player.getPlaybackState() == Player.STATE_ENDED) {
+                player.seekTo(0);
+                player.prepare();
+            }
+            player.play();
+        });
+        binding.pauseButton.setOnClickListener(v -> player.pause());
+    }
+
+    private void initializeSeekBar() {
         binding.seekbar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
             @Override
             public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
-                if (fromUser) {
-                    if (!isMediaPrepared) {
-                        if (realPath != null) {
-                            Media media = new Media(libVLC, realPath);
-                            mediaPlayer.setMedia(media);
-                            mediaPlayer.play();
-                        }
-                    }
-                    mediaPlayer.setTime((long) ((progress / (double) SEEKBAR_MAX) * mediaPlayer.getLength()));
+                if (fromUser && player != null) {
+                    player.seekTo(progress);
                 }
             }
 
             @Override
             public void onStartTrackingTouch(SeekBar seekBar) {
-                stopProgressUpdate();
+                stopProgressUpdates();
             }
 
             @Override
             public void onStopTrackingTouch(SeekBar seekBar) {
-                startProgressUpdate();
+                startProgressUpdates();
             }
         });
+
+        handler = new Handler();
+        updateProgressAction = this::updateProgress;
     }
 
-    private void startProgressUpdate() {
-        if (timer != null) {
-            timer.cancel();
+    private void startProgressUpdates() {
+        handler.post(updateProgressAction);
+    }
+
+    private void stopProgressUpdates() {
+        handler.removeCallbacks(updateProgressAction);
+    }
+
+    private void updateProgress() {
+        if (player != null && player.isPlaying()) {
+            binding.seekbar.setMax(SEEKBAR_MAX);
+            long currentPosition = player.getCurrentPosition();
+            long duration = player.getDuration();
+            int position = (int) ((currentPosition / (double) duration) * SEEKBAR_MAX);
+            binding.seekbar.setProgress(position);
+            binding.timePlay.setText(TimeUtil.formatTime(currentPosition) + " / " + TimeUtil.formatTime(duration));
+            handler.postDelayed(updateProgressAction, 1);
         }
-        timer = new Timer();
-        timer.schedule(new TimerTask() {
-            @Override
-            public void run() {
-                updateSeekbarPositionAndTimePlay();
-            }
-        }, 0, 1000);
-    }
-
-    private void updateSeekbarPositionAndTimePlay() {
-        runOnUiThread(() -> {
-            if (mediaPlayer != null) {
-                binding.seekbar.setMax(SEEKBAR_MAX);
-                long currentPosition = mediaPlayer.getTime();
-                long duration = mediaPlayer.getLength();
-                int position = (int) ((currentPosition / (double) duration) * SEEKBAR_MAX);
-                binding.seekbar.setProgress(position);
-                binding.timePlay.setText(TimeUtil.formatTime(currentPosition) + " / " + TimeUtil.formatTime(duration));
-            }
-        });
-    }
-
-    private void updateSeekbarPositionAndTimePlayToEnd(){
-        runOnUiThread(() -> {
-            if (mediaPlayer != null) {
-                binding.seekbar.setProgress(binding.seekbar.getMax());
-                long duration = mediaPlayer.getLength();
-                binding.timePlay.setText(TimeUtil.formatTime(duration) + " / " + TimeUtil.formatTime(duration));
-            }
-        });
-    }
-
-    private void stopProgressUpdate() {
-        if (timer != null) {
-            timer.cancel();
-            timer = null;
-        }
-        try {
-            updateSeekbarPositionAndTimePlay();
-        }catch (Exception ignore){}
-    }
-
-    @Override
-    protected void onPause() {
-        super.onPause();
-        if (mediaPlayer != null) {
-            mediaPlayer.detachViews();
-            savedPosition = (int) mediaPlayer.getTime();
-            mediaPlayer.pause();
-        }
-        stopProgressUpdate();
-    }
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-        mediaPlayer.attachViews(binding.videoLayout, null, false, false);
-        if (savedPosition != -1) {
-            mediaPlayer.setTime(savedPosition);
-        }
-        mediaPlayer.play();
     }
 
     @Override
     protected void onDestroy() {
         super.onDestroy();
-        if (mediaPlayer != null) {
-            mediaPlayer.stop();
-            mediaPlayer.detachViews();
-            mediaPlayer.release();
-        }
-        if (libVLC != null) {
-            libVLC.release();
-        }
-        stopProgressUpdate();
-    }
-
-    private void setupYiers() {
-        binding.videoLayout.setOnClickListener(v -> {
-            setPureVideo(!pureVideo);
-        });
-    }
-
-    private void setPureVideo(boolean purePhoto) {
-        if(purePhoto){
-            binding.appBarLayout.setVisibility(View.GONE);
-            WindowAndSystemUiUtil.setSystemUIShown(this, false);
-            this.pureVideo = true;
-        }else {
-            binding.appBarLayout.setVisibility(View.VISIBLE);
-            WindowAndSystemUiUtil.setSystemUIShown(this, true);
-            this.pureVideo = false;
+        if (player != null) {
+            stopProgressUpdates();
+            player.release();
+            player = null;
         }
     }
 }
