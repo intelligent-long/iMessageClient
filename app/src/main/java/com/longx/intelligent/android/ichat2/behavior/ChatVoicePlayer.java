03a1e1a8f63767be4d235d8c3eee127b4fba78ff
long <tx.long@outlook.com>
2024-07-20 00:50:44 +0800

    commit

--- app/src/main/java/com/longx/intelligent/android/ichat2/behavior/ChatVoicePlayer.java
+++ app/src/main/java/com/longx/intelligent/android/ichat2/behavior/ChatVoicePlayer.java
@@ -1,11 +1,20 @@
 package com.longx.intelligent.android.ichat2.behavior;
 
+import static android.content.Context.POWER_SERVICE;
+
+import android.annotation.SuppressLint;
 import android.app.Activity;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothProfile;
 import android.content.Context;
 import android.media.AudioAttributes;
+import android.media.AudioDeviceInfo;
+import android.media.AudioFocusRequest;
 import android.media.AudioManager;
 import android.media.MediaPlayer;
 import android.net.Uri;
+import android.os.Build;
+import android.os.PowerManager;
 import android.os.VibrationEffect;
 import android.os.Vibrator;
 import android.util.Log;
@@ -18,17 +27,22 @@ import android.view.HapticFeedbackConstants;
 import com.longx.intelligent.android.ichat2.activity.ChatActivity;
 import com.longx.intelligent.android.ichat2.util.ErrorLogger;
 
+import java.io.IOException;
+
 public class ChatVoicePlayer {
     private MediaPlayer mediaPlayer;
     private final ChatActivity chatActivity;
     private final AudioManager audioManager;
     private final SensorManager sensorManager;
+    private final PowerManager powerManager;
+    private PowerManager.WakeLock wakeLock;
     private final Sensor proximitySensor;
     private final SensorEventListener proximitySensorYier;
     private OnPlayStateChangeYier onPlayStateChangeYier;
     private String id;
     private Uri uri;
     private boolean earpieceNow;
+    private AudioFocusRequest audioFocusRequest;
 
     public interface OnPlayStateChangeYier {
         void onStart(String id);
@@ -41,18 +55,26 @@ public class ChatVoicePlayer {
         this.chatActivity = chatActivity;
         audioManager = (AudioManager) chatActivity.getSystemService(Context.AUDIO_SERVICE);
         sensorManager = (SensorManager) chatActivity.getSystemService(Context.SENSOR_SERVICE);
+        powerManager = (PowerManager) chatActivity.getSystemService(POWER_SERVICE);
         proximitySensor = sensorManager.getDefaultSensor(Sensor.TYPE_PROXIMITY);
 
         proximitySensorYier = new SensorEventListener() {
             @Override
             public void onSensorChanged(SensorEvent event) {
+                if (isHeadphonesPlugged() || isBluetoothAudioConnected()) {
+                    return;
+                }
                 if (proximitySensor != null) {
                     if (event.values[0] < proximitySensor.getMaximumRange()) {
-                        earpieceNow = true;
-                        switchToEarpiece();
+                        if(!earpieceNow) {
+                            earpieceNow = true;
+                            switchToEarpiece();
+                        }
                     } else {
-                        earpieceNow = false;
-                        switchToSpeaker();
+                        if(earpieceNow) {
+                            earpieceNow = false;
+                            switchToSpeaker();
+                        }
                     }
                 }
             }
@@ -61,6 +83,19 @@ public class ChatVoicePlayer {
             public void onAccuracyChanged(Sensor sensor, int accuracy) {
             }
         };
+
+        if(audioFocusRequest == null) {
+            audioFocusRequest = new AudioFocusRequest.Builder(AudioManager.AUDIOFOCUS_GAIN_TRANSIENT)
+                    .setAudioAttributes(
+                            new AudioAttributes.Builder()
+                                    .setUsage(AudioAttributes.USAGE_MEDIA)
+                                    .setContentType(AudioAttributes.CONTENT_TYPE_MUSIC)
+                                    .build()
+                    )
+                    .setAcceptsDelayedFocusGain(true)
+                    .setOnAudioFocusChangeListener(new AudioFocusChangeYier())
+                    .build();
+        }
     }
 
     public void setOnPlayStateChangeYier(OnPlayStateChangeYier listener) {
@@ -78,10 +113,17 @@ public class ChatVoicePlayer {
 
     public void play() {
         if (mediaPlayer != null && !mediaPlayer.isPlaying()) {
-            mediaPlayer.start();
-            registerProximitySensor();
-            if (onPlayStateChangeYier != null) {
-                onPlayStateChangeYier.onStart(id);
+            int result = audioManager.requestAudioFocus(audioFocusRequest);
+            if(result == AudioManager.AUDIOFOCUS_REQUEST_GRANTED) {
+                mediaPlayer.start();
+                registerProximitySensor();
+                if (onPlayStateChangeYier != null) {
+                    onPlayStateChangeYier.onStart(id);
+                }
+            }else {
+                if (onPlayStateChangeYier != null) {
+                    onPlayStateChangeYier.onError(id, -1, -1);
+                }
             }
         }
     }
@@ -89,6 +131,7 @@ public class ChatVoicePlayer {
     public void pause() {
         if (mediaPlayer != null && mediaPlayer.isPlaying()) {
             mediaPlayer.pause();
+            audioManager.abandonAudioFocusRequest(audioFocusRequest);
             unregisterProximitySensor();
             if (onPlayStateChangeYier != null) {
                 onPlayStateChangeYier.onPause(id);
@@ -100,6 +143,7 @@ public class ChatVoicePlayer {
         if (mediaPlayer != null) {
             mediaPlayer.stop();
             mediaPlayer.reset();
+            audioManager.abandonAudioFocusRequest(audioFocusRequest);
             unregisterProximitySensor();
             if (onPlayStateChangeYier != null) {
                 onPlayStateChangeYier.onStop(id, false);
@@ -119,6 +163,7 @@ public class ChatVoicePlayer {
             mediaPlayer = null;
             id = null;
         }
+        audioManager.abandonAudioFocusRequest(audioFocusRequest);
         unregisterProximitySensor();
         return state;
     }
@@ -144,16 +189,36 @@ public class ChatVoicePlayer {
     }
 
     private void switchToEarpiece() {
+        setScreenOff();
+        pause();
+        int position = getPlaybackPosition() - 1000;
+
         audioManager.setSpeakerphoneOn(false);
         audioManager.setMode(AudioManager.MODE_IN_COMMUNICATION);
         setAudioAttributes(AudioManager.STREAM_VOICE_CALL);
+
+        try {
+            Thread.sleep(1500);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+        seekTo(Math.max(position, 0));
+        play();
         Log.d(getClass().getName(), "Switched to earpiece");
     }
 
     private void switchToSpeaker() {
+        setScreenOn();
+        pause();
+        int position = getPlaybackPosition() - 1000;
+
         audioManager.setSpeakerphoneOn(true);
         audioManager.setMode(AudioManager.MODE_NORMAL);
         setAudioAttributes(AudioManager.STREAM_MUSIC);
+
+        seekTo(Math.max(position, 0));
+        play();
         Log.d(getClass().getName(), "Switched to speaker");
     }
 
@@ -176,6 +241,7 @@ public class ChatVoicePlayer {
 
     private void setupYiers() {
         mediaPlayer.setOnCompletionListener(mp -> {
+            audioManager.abandonAudioFocusRequest(audioFocusRequest);
             mediaPlayer.seekTo(getDuration());
             if (onPlayStateChangeYier != null) {
                 onPlayStateChangeYier.onStop(id, true);
@@ -185,7 +251,7 @@ public class ChatVoicePlayer {
                             HapticFeedbackConstants.FLAG_IGNORE_GLOBAL_SETTING
                     );
                     try {
-                        Thread.sleep(210);
+                        Thread.sleep(200);
                     } catch (InterruptedException e) {
                         throw new RuntimeException(e);
                     }
@@ -199,6 +265,7 @@ public class ChatVoicePlayer {
         });
 
         mediaPlayer.setOnErrorListener((mp, what, extra) -> {
+            audioManager.abandonAudioFocusRequest(audioFocusRequest);
             if (onPlayStateChangeYier != null) {
                 onPlayStateChangeYier.onError(id, what, extra);
             }
@@ -236,6 +303,62 @@ public class ChatVoicePlayer {
 
         public int getPosition() {
             return position;
+        }
+    }
+
+    private class AudioFocusChangeYier implements AudioManager.OnAudioFocusChangeListener{
+        @Override
+        public void onAudioFocusChange(int focusChange) {
+            switch (focusChange) {
+                case AudioManager.AUDIOFOCUS_LOSS:
+                case AudioManager.AUDIOFOCUS_LOSS_TRANSIENT:
+                case AudioManager.AUDIOFOCUS_LOSS_TRANSIENT_CAN_DUCK:
+                    pause();
+                    break;
+            };
+        }
+    }
+
+    private boolean isHeadphonesPlugged() {
+        if (audioManager == null) {
+            return false;
+        }
+        AudioDeviceInfo[] audioDevicesInputs = audioManager.getDevices(AudioManager.GET_DEVICES_INPUTS);
+        for (AudioDeviceInfo deviceInfo : audioDevicesInputs) {
+            if (deviceInfo.getType() == AudioDeviceInfo.TYPE_WIRED_HEADPHONES
+                    || deviceInfo.getType() == AudioDeviceInfo.TYPE_WIRED_HEADSET) {
+                return true;
+            }
+        }
+        AudioDeviceInfo[] audioDevicesOutputs = audioManager.getDevices(AudioManager.GET_DEVICES_OUTPUTS);
+        for (AudioDeviceInfo deviceInfo : audioDevicesOutputs) {
+            if (deviceInfo.getType() == AudioDeviceInfo.TYPE_WIRED_HEADPHONES
+                    || deviceInfo.getType() == AudioDeviceInfo.TYPE_WIRED_HEADSET) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private boolean isBluetoothAudioConnected() {
+        BluetoothAdapter bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+        @SuppressLint("MissingPermission") int state = bluetoothAdapter.getProfileConnectionState(BluetoothProfile.A2DP);
+        return state == BluetoothAdapter.STATE_CONNECTED;
+    }
+
+    @SuppressLint("InvalidWakeLockTag")
+    private void setScreenOff() {
+        if (wakeLock == null) {
+            wakeLock = powerManager.newWakeLock(PowerManager.PROXIMITY_SCREEN_OFF_WAKE_LOCK, getClass().getName());
+        }
+        wakeLock.acquire(30 * 60 * 1000);
+    }
+
+    private void setScreenOn() {
+        if (wakeLock != null) {
+            wakeLock.setReferenceCounted(false);
+            wakeLock.release();
+            wakeLock = null;
         }
     }
 }
