8bbdba39df3e9095cd047876b24fba64558d1bdf
long <tx.long@outlook.com>
2025-01-27 08:08:05 +0800

    commit.

--- app/src/main/java/com/longx/intelligent/android/imessage/bottomsheet/AuthMoreOperationBottomSheet.java
+++ app/src/main/java/com/longx/intelligent/android/imessage/bottomsheet/AuthMoreOperationBottomSheet.java
@@ -7,7 +7,9 @@ import androidx.appcompat.app.AppCompatActivity;
 import com.longx.intelligent.android.imessage.activity.OfflineDetailsActivity;
 import com.longx.intelligent.android.imessage.activity.VersionActivity;
 import com.longx.intelligent.android.imessage.databinding.BottomSheetAuthMoreOperationBinding;
+import com.longx.intelligent.android.imessage.dialog.ConfirmDialog;
 import com.longx.intelligent.android.imessage.dialog.ServerSettingDialog;
+import com.longx.intelligent.android.imessage.util.AppUtil;
 
 /**
  * Created by LONG on 2024/3/27 at 3:54 PM.
@@ -39,6 +41,14 @@ public class AuthMoreOperationBottomSheet extends AbstractBottomSheet{
         binding.offlineDetail.setOnClickListener(v -> {
             dismiss();
             getActivity().startActivity(new Intent(getActivity(), OfflineDetailsActivity.class));
+        });
+        binding.restart.setOnClickListener(v -> {
+            new ConfirmDialog(getActivity(), "如果应用出现异常，重新启动可能可以解决问题。\n是否确定要继续？\n注意：此操作有极低概率导致数据异常。")
+                    .setNegativeButton()
+                    .setPositiveButton((dialog, which) -> {
+                        AppUtil.restartApp(getActivity());
+                    })
+                    .create().show();
         });
     }
 }
