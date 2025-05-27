22f079aa49022db34ee5f6a0f1e8f6fa73beaf96
long <tx.long@outlook.com>
2025-05-27 23:08:30 +0800

    commit.

--- app/src/main/java/com/longx/intelligent/android/imessage/activity/ExploreGroupChannelActivity.java
+++ app/src/main/java/com/longx/intelligent/android/imessage/activity/ExploreGroupChannelActivity.java
@@ -1,17 +1,28 @@
 package com.longx.intelligent.android.imessage.activity;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.ArrayAdapter;
 
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+
+import com.journeyapps.barcodescanner.ScanOptions;
 import com.longx.intelligent.android.imessage.R;
 import com.longx.intelligent.android.imessage.activity.helper.BaseActivity;
 import com.longx.intelligent.android.imessage.behaviorcomponents.MessageDisplayer;
+import com.longx.intelligent.android.imessage.data.Channel;
+import com.longx.intelligent.android.imessage.data.ChannelQrCode;
 import com.longx.intelligent.android.imessage.data.GroupChannel;
+import com.longx.intelligent.android.imessage.data.GroupChannelQrCode;
+import com.longx.intelligent.android.imessage.data.QrCodeData;
 import com.longx.intelligent.android.imessage.data.response.OperationData;
 import com.longx.intelligent.android.imessage.databinding.ActivityExploreGroupChannelBinding;
+import com.longx.intelligent.android.imessage.net.retrofit.caller.ChannelApiCaller;
 import com.longx.intelligent.android.imessage.net.retrofit.caller.GroupChannelApiCaller;
 import com.longx.intelligent.android.imessage.net.retrofit.caller.RetrofitApiCaller;
+import com.longx.intelligent.android.imessage.util.ErrorLogger;
 import com.longx.intelligent.android.imessage.util.UiUtil;
 
 import retrofit2.Call;
@@ -20,6 +31,7 @@ import retrofit2.Response;
 public class ExploreGroupChannelActivity extends BaseActivity {
     private ActivityExploreGroupChannelBinding binding;
     private String[] searchByNames;
+    private ActivityResultLauncher<Intent> qrScanLauncher;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,7 +41,52 @@ public class ExploreGroupChannelActivity extends BaseActivity {
         setContentView(binding.getRoot());
         setupDefaultBackNavigation(binding.toolbar);
         setupViews();
+        registerResultLauncher();
         setupYiers();
+    }
+
+    private void registerResultLauncher() {
+        qrScanLauncher = registerForActivityResult(
+                new ActivityResultContracts.StartActivityForResult(),
+                result -> {
+                    String qrContent = null;
+                    if (result.getResultCode() == Activity.RESULT_OK && result.getData() != null) {
+                        Intent data = result.getData();
+                        qrContent = data.getStringExtra("SCAN_RESULT");
+                        if (qrContent == null) {
+                            Bundle extras = data.getExtras();
+                            if (extras != null) {
+                                qrContent = extras.get("SCAN_RESULT").toString();
+                            }
+                        }
+                    }
+                    if (qrContent != null){
+                        GroupChannelQrCode groupChannelQrCode = null;
+                        try {
+                            QrCodeData<?> qrCodeData = QrCodeData.toObject(qrContent);
+                            groupChannelQrCode = qrCodeData.getData(GroupChannelQrCode.class);
+                        }catch (Exception e){
+                            ErrorLogger.log(e);
+                            MessageDisplayer.autoShow(this, "二维码解析失败", MessageDisplayer.Duration.SHORT);
+                        }
+                        if(groupChannelQrCode != null) {
+                            GroupChannelApiCaller.findGroupChannelByGroupChannelId(this, groupChannelQrCode.getGroupChannelId(), new RetrofitApiCaller.DelayedShowDialogCommonYier<OperationData>(this) {
+                                @Override
+                                public void ok(OperationData data, Response<OperationData> raw, Call<OperationData> call) {
+                                    super.ok(data, raw, call);
+                                    data.commonHandleResult(ExploreGroupChannelActivity.this, new int[]{-101}, () -> {
+                                        GroupChannel groupChannel = data.getData(GroupChannel.class);
+                                        Intent intent = new Intent(ExploreGroupChannelActivity.this, GroupChannelActivity.class);
+                                        intent.putExtra(ExtraKeys.GROUP_CHANNEL, groupChannel);
+                                        intent.putExtra(ExtraKeys.MAY_NOT_ASSOCIATED, true);
+                                        startActivity(intent);
+                                    });
+                                }
+                            });
+                        }
+                    }
+                }
+        );
     }
 
     private void setupViews() {
@@ -62,7 +119,10 @@ public class ExploreGroupChannelActivity extends BaseActivity {
                     }
                 });
             } else if (item.getItemId() == R.id.search_by_qr_code) {
-
+                ScanOptions options = new ScanOptions();
+                options.setPrompt("请将群频道二维码置于取景框内扫描。");
+                options.setCaptureActivity(QrScanActivity.class);
+                qrScanLauncher.launch(options.createScanIntent(this));
             }
             return true;
         });
